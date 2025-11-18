b3e950c7c09bfa102ac4c35d965ce0e779756288
lisaag <lisatgroen@hotmail.com>
2025-11-18 14:38:42 +0100

    changed website title and icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.scss";
 import Main from "./containers/Main";
+import React, { useEffect } from 'react';
+
+function Example() {
+  useEffect(() => {
+    document.title = "Lisa's Portfolio";
+  }, []);
+}
 
 function App() {
+  Example()
   return (
     <div>
+      
       <Main />
     </div>
   );
